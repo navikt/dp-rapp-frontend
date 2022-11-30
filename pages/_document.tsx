f750e45e620dd2d4e3d0acba261cb814f7694b2e
Igor Shuliakov <igor.shuliakov@nav.no>
2022-11-30 13:49:23 +0100

    NAV Dekoratøren

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,69 @@
-import { Html, Head, Main, NextScript } from "next/document";
-export default function Document() {
-  return (
-    <Html>
-      <Head></Head>
-      <body>
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
+import { JSDOM } from "jsdom";
+import { ReactElement } from "react";
+
+type DocumentProps = DocumentInitialProps & {
+  dStyles: ReactElement;
+  dScripts: ReactElement;
+  dHeader: ReactElement;
+  dFooter: ReactElement;
+}
+
+class MyDocument extends Document<DocumentProps> {
+  static createElement(dom: JSDOM, elementId: string): ReactElement {
+    return <div dangerouslySetInnerHTML={{ __html: dom.window.document.getElementById(elementId)?.innerHTML || '' }} />
+  }
+
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentProps> {
+    const originalRenderPage = ctx.renderPage
+
+    const response = await fetch("https://www.dev.nav.no/dekoratoren/");
+    const text = await response.text();
+    const dom = new JSDOM(text);
+
+    const dStyles = this.createElement(dom, "styles");
+    const dScripts = this.createElement(dom, "scripts");
+    const dHeader = this.createElement(dom, "header-withmenu");
+    const dFooter = this.createElement(dom, "footer-withmenu");
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => (props) => <App {...props} />,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      });
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    // return initialProps + additional props
+    return {
+      ...initialProps,
+      dStyles,
+      dScripts,
+      dHeader,
+      dFooter
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {this.props.dStyles}
+          {this.props.dScripts}
+        </Head>
+        <body>
+        {this.props.dHeader}
         <Main />
         <NextScript />
-      </body>
-    </Html>
-  );
+        {this.props.dFooter}
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
