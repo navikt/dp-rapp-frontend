c229f5cc8aa928bacef5933b4e7eb16ed0181fb6
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-06 10:36:27 +0100

    belaget knapp trenger å legge til publicruntimeconfig for å hente injecte env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@navikt/ds-react";
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+  const kallApi = () =>
+    fetch("/api/hello")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  const kallRappApi = () => console.log("denne gjør ingenting enda!!");
+  return (
+    <main>
+      <h1>Hello, Next.js!</h1>
+      <Button onClick={() => kallApi()}> kall hello api </Button>
+      <Button onClick={() => kallRappApi()}> kall dp-rapp-api </Button>
+    </main>
+  );
 }
