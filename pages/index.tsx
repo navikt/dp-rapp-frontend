cec46e7b1a83e1197b7c7f0208d584390ac9dc14
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-06 12:49:00 +0100

    lagt til debug av publicruntimeconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@navikt/ds-react";
 import { kallRappApiBak } from "./api/rapp";
+import getConfig from "next/config";
+import { Env } from "@navikt/nav-dekoratoren-moduler/ssr";
 
 export default function Page() {
   const kallApi = () =>
@@ -10,9 +12,15 @@ export default function Page() {
     kallRappApiBak()
       .then((response) => response.json())
       .then((data) => console.log(data));
+  const { publicRuntimeConfig } = getConfig();
+  const dekoratorEnv = publicRuntimeConfig.NAV_DEKORATOREN_ENV as Exclude<
+    Env,
+    "localhost"
+  >;
   return (
     <main>
       <h1>Hello, Next.js!</h1>
+      <p>dekoratoren env er: {dekoratorEnv}</p>
       <Button onClick={() => kallApi()}> kall hello api </Button>
       <Button onClick={() => kallRappApi()}> kall dp-rapp-api </Button>
     </main>
