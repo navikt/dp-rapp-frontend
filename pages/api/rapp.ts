8807179c2f0c48ec58f08986e215f26e610b75d7
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-07 12:50:35 +0100

    sender til authenticated ping uten ordentlige headers

--- pages/api/rapp.ts
+++ pages/api/rapp.ts
@@ -17,8 +17,9 @@ export default async function handler(
 
   //const callId = uuid();
   const dpRappApiUrl = process.env.DP_RAPP_API_URL;
-  const url = `${dpRappApiUrl}/api/v1/ping`;
+  const url = `${dpRappApiUrl}/api/v1/authenticatedping`;
   console.log("Prøver å pinge api at url: " + url);
+  console.log("headers: " + JSON.stringify(req.headers));
   const response = await fetch(url);
   return res.json(response);
 }
