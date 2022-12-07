6f3c55f70561ddf2d67b237f455ca03fc948a2f8
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-07 13:17:41 +0100

    fjerne consolelog

--- pages/api/rapp.ts
+++ pages/api/rapp.ts
@@ -18,7 +18,6 @@ export default async function handler(
   //const callId = uuid();
   const dpRappApiUrl = process.env.DP_RAPP_API_URL;
   const url = `${dpRappApiUrl}/api/v1/authenticatedping`;
-  console.log("Prøver å pinge api at url: " + url);
   const response = await fetch(url, {
     headers: { Authorization: req.headers.authorization },
   });
