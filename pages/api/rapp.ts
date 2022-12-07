5521d835f613abf7c9fa4a94ee4cb430b9aa27b4
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-07 09:42:45 +0100

    bruke internt hostname for zero trust

--- pages/api/rapp.ts
+++ pages/api/rapp.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { GetServerSidePropsContext } from "next";
-import getConfig from "next/config";
 
 type Data = {
   name: string;
@@ -12,6 +11,7 @@ export default async function handler({ req }: GetServerSidePropsContext) {
   //const callId = uuid();
   const dpRappApiUrl = process.env.DP_RAPP_API_URL;
   const url = `${dpRappApiUrl}/api/v1/ping`;
+  console.log("Prøver å pinge api at url: " + url);
 
   return await fetch(url, {
     headers: {
