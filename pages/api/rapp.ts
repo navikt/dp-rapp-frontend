4d8b1674de08dbc4fd6b77003253d8f2bacddb39
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-07 11:01:29 +0100

    prøver oss på nextapiresponse igjen

--- pages/api/rapp.ts
+++ pages/api/rapp.ts
@@ -1,24 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { GetServerSidePropsContext } from "next";
+import type {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
 
 type Data = {
   name: string;
 };
 
-export default async function handler({ req }: GetServerSidePropsContext) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   //const { token } = await getSession(req);
 
   //const callId = uuid();
   const dpRappApiUrl = process.env.DP_RAPP_API_URL;
   const url = `${dpRappApiUrl}/api/v1/ping`;
   console.log("Prøver å pinge api at url: " + url);
-
-  return await fetch(url, {
-    headers: {
-      //Authorization: `Bearer ${token}`,
-      //"Downstream-Authorization": `Bearer ${token}`,
-      "Nav-Consumer-Id": "dp-rapp-frontend",
-      //"Nav-Call-Id": callId,
-    },
-  });
+  const response = await fetch(url);
+  return res.json(response);
 }
