8c8bbbbbd3279eec48ab4a13f06849412216d9a5
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-09 14:46:05 +0100

    fjerner console logs

--- src/pages/api/rapp.ts
+++ src/pages/api/rapp.ts
@@ -6,20 +6,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  //const { token } = await getSession(req);
-  console.log("1: fått kall");
-  const audienceRappApi = `dev-gcp:raptus:dp-rapp-api`;
+  const audienceRappApi = `${process.env.NAIS_CLUSTER_NAME}:raptus:dp-rapp-api`;
   try {
-    console.log("2: prøver å hente session");
     const session = await getSession(req);
 
     if (!session) {
-      return res.status(401).end();
+      return res.status(401).end(); //skal aldri skje siden vi har wonderwall med enforcedlogin
     }
-    console.log("3: lager obo token");
     const onBehalfOfToken = await session.apiToken(audienceRappApi);
 
-    //const callId = uuid();
+    //const callId = uuid(); //trengs kanskje senere hvis vi skal ha tracing
     const dpRappApiUrl = process.env.DP_RAPP_API_URL;
     const url = `${dpRappApiUrl}/api/v1/authenticatedping`;
     console.log("4: pinger rapp-api");
@@ -31,13 +27,11 @@ export default async function handler(
         Authorization: `Bearer ${onBehalfOfToken}`,
       },
     });
-    console.log("5: fått respons fra api: " + response.statusText);
     if (!response.ok) {
       throw new Error(`unexpected response ${response.statusText}`);
     }
     return res.json(response);
   } catch (error) {
-    console.log("aiii, noe kresjet. error: " + error);
     return res.status(500).send(error);
   }
 }
