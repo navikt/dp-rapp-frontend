2c3c2bfb49f8137e8efc80665b2b5cd153ab9747
Knut Magne Riise <knut.magne.riise@nav.no>
2022-12-09 15:18:01 +0100

    fix auth-utils til å bruke const

--- src/auth.utils.ts
+++ src/auth.utils.ts
@@ -2,9 +2,7 @@ import { GetSessionWithOboProvider, makeSession } from "@navikt/dp-auth";
 import { idporten } from "@navikt/dp-auth/identity-providers";
 import { tokenX, withInMemoryCache } from "@navikt/dp-auth/obo-providers";
 
-let getSession: GetSessionWithOboProvider;
-
-getSession = makeSession({
+const getSession: GetSessionWithOboProvider = makeSession({
   identityProvider: idporten,
   oboProvider: withInMemoryCache(tokenX),
 });
