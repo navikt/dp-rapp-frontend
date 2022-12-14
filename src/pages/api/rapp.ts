a2c24ed250574e338d2db7302df9d721a48af734
Igor Shuliakov <igor.shuliakov@nav.no>
2022-12-14 14:13:07 +0100

    Sender X-Request-ID

--- src/pages/api/rapp.ts
+++ src/pages/api/rapp.ts
@@ -1,21 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "../../auth.utils";
+import { v4 as uuid } from 'uuid';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const audienceRappApi = `${process.env.NAIS_CLUSTER_NAME}:raptus:dp-rapp-api`;
   try {
     const session = await getSession(req);
 
     if (!session) {
       return res.status(401).end(); //skal aldri skje siden vi har wonderwall med enforcedlogin
     }
+
+    const audienceRappApi = `${process.env.NAIS_CLUSTER_NAME}:raptus:dp-rapp-api`;
+
     const onBehalfOfToken = await session.apiToken(audienceRappApi);
 
-    //const callId = uuid(); //trengs kanskje senere hvis vi skal ha tracing
+    const callId = uuid();
     const dpRappApiUrl = process.env.DP_RAPP_API_URL;
     const url = `${dpRappApiUrl}/api/v1/authenticatedping`;
     const response = await fetch(url, {
@@ -24,11 +26,14 @@ export default async function handler(
         "Content-Type": "application/json",
         Accept: "application/json",
         Authorization: `Bearer ${onBehalfOfToken}`,
+        "X-Request-ID": callId
       },
-    });
+    })
+
     if (!response.ok) {
-      throw new Error(`unexpected response ${response.statusText}`);
+      return res.status(500).send(`Unexpected response status: ${response.statusText}`)
     }
+
     return res.json(response);
   } catch (error) {
     return res.status(500).send(error);
