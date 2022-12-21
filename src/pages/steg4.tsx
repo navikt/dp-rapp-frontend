000406f3ebdbe94015f1ba683dab43def12867bc
Igor Shuliakov <igor.shuliakov@nav.no>
2022-12-21 21:20:04 +0100

    Steg4 form check

--- src/pages/steg4.tsx
+++ src/pages/steg4.tsx
@@ -1,14 +1,61 @@
 import { ConfirmationPanel, Heading } from "@navikt/ds-react";
-import NavPanel from "../../components/NavPanel";
+import Divider from "../../components/Divider";
 import CustomStepper from "../../components/CustomStepper";
 import Spacer from "../../components/Spacer";
-import Divider from "../../components/Divider";
-import { useState } from "react";
+import NavPanelWithSubmit from "../../components/NavPanelWithSubmit";
 import CancelButton from "../../components/CancelButton";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Page() {
 
-  const [state, setState] = useState(false);
+  const [check, setCheck] = useState<boolean>(false);
+  const [val6, setVal6] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (event: FormEvent) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    setCheck(true);
+
+    if (!val6) {
+      return false;
+    }
+
+    const data = {
+      sp6: val6,
+    }
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data)
+
+    // API endpoint where we send form data.
+    const endpoint = '/api/form'
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    }
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options)
+
+    // Get the response data from server as JSON.
+    // const result = await response.json()
+
+    console.log(response.ok);
+
+    await router.push('/');
+  }
 
   return (
     <main>
@@ -27,19 +74,21 @@ export default function Page() {
 
       <Spacer />
 
-      <ConfirmationPanel
-        checked={state}
-        label="Ja, jeg samtykker."
-        onChange={() => setState((x) => !x)}
-        error={!state && "Du må samtykke før du kan fortsette."}
-      >
-        For å komme videre må du gi oss lov til å hente inn og bruke opplysninger
-        om deg.
-      </ConfirmationPanel>
+      <form onSubmit={handleSubmit}>
+        <ConfirmationPanel
+          checked={val6}
+          label="Ja, jeg samtykker."
+          onChange={() => setVal6((x) => !x)}
+          error={check && !val6 && "Du må samtykke før du kan fortsette."}
+        >
+          For å komme videre må du gi oss lov til å hente inn og bruke opplysninger
+          om deg.
+        </ConfirmationPanel>
 
-      <Spacer />
+        <Spacer />
 
-      <NavPanel backHref="/steg3" backText="Forrige steg" nextHref="/send" nextText="Send" />
+        <NavPanelWithSubmit backHref="/steg2" backText="Forrige steg" nextText="Neste steg" />
+      </form>
 
       <CancelButton />
     </main>
