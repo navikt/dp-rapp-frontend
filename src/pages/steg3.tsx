45768a995870bd8cb333068363b763f47a9788a1
Igor Shuliakov <igor.shuliakov@nav.no>
2022-12-21 21:19:56 +0100

    Steg3 form check

--- src/pages/steg3.tsx
+++ src/pages/steg3.tsx
@@ -1,14 +1,61 @@
 import { Heading, Radio, RadioGroup } from "@navikt/ds-react";
-import NavPanel from "../../components/NavPanel";
+import Divider from "../../components/Divider";
 import CustomStepper from "../../components/CustomStepper";
 import Spacer from "../../components/Spacer";
-import Divider from "../../components/Divider";
-import { useState } from "react";
+import NavPanelWithSubmit from "../../components/NavPanelWithSubmit";
 import CancelButton from "../../components/CancelButton";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Page() {
 
-  const [val1, setVal1] = useState<boolean | null>(null);
+  const [check, setCheck] = useState<boolean>(false);
+  const [val5, setVal5] = useState<boolean | null>(null);
+
+  const router = useRouter();
+
+  const handleSubmit = async (event: FormEvent) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    setCheck(true);
+
+    if (val5 == undefined) {
+      return false;
+    }
+
+    const data = {
+      sp5: val5,
+    }
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data)
+
+    // API endpoint where we send form data.
+    const endpoint = '/api/form'
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    }
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options)
+
+    // Get the response data from server as JSON.
+    // const result = await response.json()
+
+    console.log(response.ok);
+
+    await router.push('/steg4');
+  }
 
   return (
     <main>
@@ -23,20 +70,22 @@ export default function Page() {
 
       <Spacer />
 
-      <RadioGroup
-        legend="Ønsker du fortsatt å være registrert hos NAV som arbeidssøker de neste 14 dager?"
-        description="For å få bistand og/eller ytelser, må du være registrert hos NAV"
-        onChange={(val: boolean) => setVal1(val)}
-        value={val1}
-        error=""
-      >
-        <Radio value={true}>Ja</Radio>
-        <Radio value={false}>Nei</Radio>
-      </RadioGroup>
+      <form onSubmit={handleSubmit}>
+        <RadioGroup
+          legend="Ønsker du fortsatt å være registrert hos NAV som arbeidssøker de neste 14 dager?"
+          description="For å få bistand og/eller ytelser, må du være registrert hos NAV"
+          onChange={(val: boolean) => setVal5(val)}
+          value={val5}
+          error={check && val5 == undefined && "Du må svare på dette spørsmålet"}
+        >
+          <Radio value={true}>Ja</Radio>
+          <Radio value={false}>Nei</Radio>
+        </RadioGroup>
 
-      <Spacer />
+        <Spacer />
 
-      <NavPanel backHref="/steg2" backText="Forrige steg" nextHref="/steg4" nextText="Neste steg" />
+        <NavPanelWithSubmit backHref="/steg2" backText="Forrige steg" nextText="Neste steg" />
+      </form>
 
       <CancelButton />
     </main>
