7263bdb9fb71abe47a24bc8f3c07766362dba6a6
Igor Shuliakov <igor.shuliakov@nav.no>
2022-12-27 15:10:02 +0100

    Oppsummering Steg 4

--- components/Step4.tsx
+++ components/Step4.tsx
@@ -1,13 +1,20 @@
-import { ConfirmationPanel, Heading } from "@navikt/ds-react";
+import { BodyShort, ConfirmationPanel, Heading, Panel } from "@navikt/ds-react";
 import Spacer from "./Spacer";
 import Error from "./Error";
 import NavPanelWithButtons from "./NavPanelWithButtons";
 import { CommonFormProps } from "../src/pages/form";
 import { FormEvent, useState } from "react";
+import { format } from "date-fns";
 
 export default function Step4(props: CommonFormProps) {
 
   const {
+    questionWork,
+    questionMeasures,
+    questionIllness,
+    questionVacation,
+    savedDates,
+    questionProceed,
     questionConsent,
     setQuestionConsent,
     prevStep,
@@ -32,6 +39,20 @@ export default function Step4(props: CommonFormProps) {
     }
   }
 
+  const days = [];
+  for (const key in savedDates) {
+    const date = format(new Date(+key), "dd.MM.yy");
+    const type = savedDates[key].type;
+    const hours = type == 'work' ? '(' + savedDates[key].hours + ' t)' : '';
+    const str = date + ' ' + type + ' ' + hours;
+
+    days.push(
+      <div key={key}>
+        {str}
+      </div>
+    );
+  }
+
   // Render
   return (
     <>
@@ -39,7 +60,54 @@ export default function Step4(props: CommonFormProps) {
 
       <Spacer />
 
-      Oppsummering. Du har valgt... Bla bla bla.
+      <Panel>
+        <Heading spacing level="4" size="small">
+          Har du vært i arbeid de siste 14 dager?
+        </Heading>
+        <BodyShort>
+          {questionWork ? 'Ja' : 'Nei'}
+        </BodyShort>
+      </Panel>
+      <Panel>
+        <Heading spacing level="4" size="small">
+          Har du deltatt på tiltak, kurs eller utdanning?
+        </Heading>
+        <BodyShort>
+          {questionMeasures ? 'Ja' : 'Nei'}
+        </BodyShort>
+      </Panel>
+      <Panel>
+        <Heading spacing level="4" size="small">
+          Har du vært syk?
+        </Heading>
+        <BodyShort>
+          {questionIllness ? 'Ja' : 'Nei'}
+        </BodyShort>
+      </Panel>
+      <Panel>
+        <Heading spacing level="4" size="small">
+          Har du hatt ferie eller annet fravær?
+        </Heading>
+        <BodyShort>
+          {questionVacation ? 'Ja' : 'Nei'}
+        </BodyShort>
+      </Panel>
+      <Panel>
+        <Heading spacing level="4" size="small">
+          Registrerte dager
+        </Heading>
+        <BodyShort>
+          {days}
+        </BodyShort>
+      </Panel>
+      <Panel>
+        <Heading spacing level="4" size="small">
+          Ønsker du fortsatt å være registrert hos NAV som arbeidssøker de neste 14 dager?
+        </Heading>
+        <BodyShort>
+          {questionProceed ? 'Ja' : 'Nei'}
+        </BodyShort>
+      </Panel>
 
       <Spacer />
 
