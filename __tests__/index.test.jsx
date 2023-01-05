5c8e578b64c909ac6bc93a3ef99f0a0e74e5f8e7
Igor Shuliakov <igor.shuliakov@nav.no>
2023-01-05 13:01:43 +0100

    Meldeperiode

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,34 +1,41 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../src/pages/index";
 import "@testing-library/jest-dom";
-import mockRouter from 'next-router-mock';
+import fetchMock from "jest-fetch-mock";
+// import mockRouter from 'next-router-mock';
+
+fetchMock.enableMocks();
+
 
 jest.mock('next/router', () => require('next-router-mock'));
 
 jest.mock('react-i18next', () => ({
-  useTranslation: () => {
-    return {
-      t: (str) => "Ny løsning",
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-      },
-    };
-  },
+    useTranslation: () => {
+        return {
+            t: (str) => "Ny løsning",
+            i18n: {
+                changeLanguage: () => new Promise(() => {
+                }),
+            },
+        };
+    },
 }));
 
 describe("Home", () => {
 
-  beforeEach(() => {
-    mockRouter.locale = "nb"
-  });
-
-  it("renders a heading", () => {
-    render(<Home />);
-
-    const heading = screen.getByRole("heading", {
-      name: /Ny løsning/i,
+    beforeEach(() => {
+        fetch.resetMocks();
     });
 
-    expect(heading).toBeInTheDocument();
-  });
+    it("renders a heading", () => {
+        // fetch.mockResponseOnce(JSON.stringify([]));
+
+        render(<Home periods={[]} />);
+
+        const heading = screen.getByRole("heading", {
+            name: /Ny løsning/i,
+        });
+
+        expect(heading).toBeInTheDocument();
+    });
 });
